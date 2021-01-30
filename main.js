67f7c123d7707f157460f14bf152950f8b7c8e60
Dessi Costa <desiree.costa@student.kyh.se>
2021-01-30 09:25:22 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+function handleClick() {
+    var myLinks = document.getElementById("myLinks");
+    if (myLinks.style.display === "block") {
+        myLinks.style.display = "none";
+    } else {
+        myLinks.style.display = "block";
+    }
+  }
+
+
+  var collapse = document.getElementsByClassName("collapse");
+
+
+for (var i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+/*   function handleClick(){
+  
+for (var i = 0; i < myLinks.length; i++) {
+  var myLinks = document.getElementsByClassName("myLinks");
+    if (myLinks.style.display === "block") {
+        myLinks.style.display = "none";
+    } else {
+        myLinks.style.display = "block";
+    }
+}
+  } */
